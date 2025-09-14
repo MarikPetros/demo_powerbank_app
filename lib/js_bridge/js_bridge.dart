c59e1c40063a0c94ed56f95f2956213d7a1a0894
MarikPetros <petrossianmarik871@gmail.com>
2025-09-14 23:14:22 +0400

    do some cleaning

--- lib/js_bridge/js_bridge.dart
+++
@@ -1,1 +0,0 @@
-export 'js_bridge_stub.dart' if (dart.library.js) 'js_bridge_web.dart';
--- lib/js_bridge/js_bridge_stub.dart
+++
@@ -1,7 +0,0 @@
-void registerApplePayCallback(Function(String) onNonce) {
-  // No-op on mobile
-}
-
-void startApplePay(String clientToken) {
-  // No-op on mobile
-}
