e494f4fbef3103261ce71c09ded2d904bafe0b49
MarikPetros <petrossianmarik871@gmail.com>
2025-09-21 22:41:01 +0400

    clear code

--- lib/screens/success_screen.dart
+++ lib/screens/success_screen.dart
@@ -1,34 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:url_launcher/url_launcher.dart';
-//
-// class SuccessScreen extends StatelessWidget {
-//   const SuccessScreen({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Text('Power Bank Dispensed!', style: TextStyle(fontSize: 24)),
-//             SizedBox(height: 20),
-//             ElevatedButton(
-//               onPressed: () async {
-//                 final url = Uri.parse('https://apps.apple.com/ru/app/recharge-city/id1594160460');
-//                 if (await canLaunchUrl(url)) {
-//                   await launchUrl(url);
-//                 }
-//               },
-//               child: Text('Download App'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
